bd8a93e8c67e7d024c04909db886460ca1f978b3
Soorya <sooryavasan@gmail.com>
2021-08-01 23:26:10 +0530

    test git changed file

--- run_test.sh
+++ run_test.sh
@@ -2,5 +2,5 @@ echo "Sh file Working"
 yq e '.dirs' coverignore.yaml
 echo $1
 export FILES_CHANGED="'$(echo $1 | tr [:blank:] "|")'"
-(grep -E $FILES_CHANGED ff.txt || true) > fg.txt
+(grep -E $FILES_CHANGED coverreport.text || true) > fg.txt
 cat fg.txt
