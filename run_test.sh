ff1a05b469f3cc42d2767d429993ecdc47d78bce
Soorya <sooryavasan@gmail.com>
2021-08-02 05:46:55 +0530

    test git changed file

--- run_test.sh
+++ run_test.sh
@@ -4,6 +4,4 @@ echo $1
 export FILES_CHANGED="'$(echo $1 | tr [:blank:] "|")'"
 echo $FILES_CHANGED
 cat coverreport.text
-grep $FILES_CHANGED coverreport.text || true
-(grep -E $FILES_CHANGED coverreport.text || true) > fg.txt
-cat fg.txt
+grep $FILES_CHANGED coverreport.text
