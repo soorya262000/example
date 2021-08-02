c13035de936d5f6ef2d4b9780d560c90603b1076
Soorya <sooryavasan@gmail.com>
2021-08-02 06:24:30 +0530

    test git changed file

--- run_test.sh
+++ run_test.sh
@@ -4,9 +4,11 @@ export FILES_CHANGED="'$(echo $@ | tr [:blank:] "|")'"
 echo $FILES_CHANGED
 cat coverreport.text
 echo "Changed"
+echo "mode: count\n" >> gh.txt
 for added_modified_file in "$@"; do
     echo "Do something with this ${added_modified_file}."
     (grep $added_modified_file coverreport.text || true) >> gh.txt
 done
+echo "File Content"
 cat gh.txt
 go tool cover -func gh.txt
--- stringutil/rev.go
+++ stringutil/rev.go
@@ -4,5 +4,6 @@ import "fmt"
 
 func Dum3(s string) string {
 	fmt.Println("afaadas")
+	fmt.Println("afaadas")
 	return s
 }
