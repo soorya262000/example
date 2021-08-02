ad15ece063e699d8fc3b6bbda0c6336a12afa38a
Soorya <sooryavasan@gmail.com>
2021-08-02 06:18:07 +0530

    test git changed file

--- run_test.sh
+++ run_test.sh
@@ -1,16 +1,11 @@
 echo "Sh file Working"
 yq e '.dirs' coverignore.yaml
-echo $1
-for added_modified_file in "$@"; do
-    echo "Do something with this ${added_modified_file}."
-done
 export FILES_CHANGED="'$(echo $@ | tr [:blank:] "|")'"
 echo $FILES_CHANGED
-grep $1 coverreport.text
+cat coverreport.text
+echo "Changed"
 for added_modified_file in "$@"; do
     echo "Do something with this ${added_modified_file}."
     (grep $added_modified_file coverreport.text || true) >> gh.txt
 done
-echo "CHanged"
 cat gh.txt
-grep -E "$FILES_CHANGED"
---
+++ stringutil/rev.go
@@ -0,0 +1,5 @@
+package stringutil
+
+func Dum3(s string) string {
+	return s
+}
